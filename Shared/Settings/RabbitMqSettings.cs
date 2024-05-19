ea7b61e7c2db5a7f05ec21105a722c2759dd0ef4
akifkemalaksu <akifkemalaksu@gmail.com>
2024-05-20 01:02:32 +0300

    Add project files.

---
+++ Shared/Settings/RabbitMqSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Settings
+{
+    public class RabbitMqSettings
+    {
+        public required string Host { get; set; }
+
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
