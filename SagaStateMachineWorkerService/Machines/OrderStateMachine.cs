913ea4bd48bab11d261237f131597871a5c4dd6b
akifkemalaksu <akifkemalaksu@gmail.com>
2024-07-06 19:02:25 +0300

    Added `SetCompletedWhenFinalized` method to `OrderStateMachine`.

--- SagaStateMachineWorkerService/Machines/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Machines/OrderStateMachine.cs
@@ -144,6 +144,8 @@ namespace SagaStateMachineWorkerService.Machines
                 .Then(context => Console.WriteLine($"{nameof(PaymentFailedEvent)} after: {context.Saga}"))
 
             );
+
+            SetCompletedWhenFinalized();
         }
     }
 }
